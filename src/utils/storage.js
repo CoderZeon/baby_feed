61027698e785c4da380711758b793101f282ab18
CoderZeon <40382534+CoderZeon@users.noreply.github.com>
2025-05-12 17:34:48 +0800

    Delete src directory

--- src/utils/storage.js
+++
@@ -1,7 +0,0 @@
-export const getCustomTitle = () => {
-  return localStorage.getItem('customTitle') || '宝宝成长日记';
-};
-
-export const setCustomTitle = (title) => {
-  localStorage.setItem('customTitle', title);
-};
